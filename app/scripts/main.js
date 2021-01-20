47decf7d31b2585120d12d7e0a293f4f7be4e0e7
kavinrajsi <sikavinraj@gmail.com>
2021-01-20 16:51:55 +0530

    participate now button trigger added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,14 @@ console.log('\'Allo \'Allo!');
 
 
 jQuery(document).ready(function ($) {
-	// remove empty div
-	$("p:empty").remove();
-
- });
+  // remove empty div
+  $("p:empty").remove();
+  // click move
+  $("#participate-button").on('click', function (e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: ($(target).offset().top)
+    }, 2000);
+  });
+});
